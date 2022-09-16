6cff03e23bf70b3ce0c4944744b60d1d6112bed2
Hunter102 <40743716+Hunter102@users.noreply.github.com>
2022-09-16 16:25:39 -0400

    SplashScreenFades

--- FruitNinja/Assets/Scripts/TitleScreen.cs
+++ FruitNinja/Assets/Scripts/TitleScreen.cs
@@ -9,47 +9,35 @@ public class TitleScreen : MonoBehaviour
 {
     public float fov = 130f;
     public float changeMultiplier = .006f;
-    public float timer = 8f;
-    public float time;
+    public float endFOV;
 
-    public GameObject splashScreen;
-    public GameObject intro;
-
-    //public bool awake;
-
-    //public Image fadeImage;
+    public Image fadeImage;
 
 
     void Start()
     {
-        //Camera.main.fieldOfView = fov;
-       // awake = true;
-        //Zoom(awake);
+        
+        Camera.main.fieldOfView = fov;
     }
     void Update()
     {
-        time += Time.deltaTime;
-        if (fov <= 170)
+        
+        if (fov < endFOV)
         {
-            Camera.main.fieldOfView = fov;
-            fov += changeMultiplier;
+        Camera.main.fieldOfView = fov;
+        fov += changeMultiplier;
         }
-        else if (time > timer)
-        {
-            intro.SetActive(false);
-            splashScreen.SetActive(true);
-            //awake = false;
-            //Fade();
+        else{
+        StartCoroutine(Fade());
         }
     }
 
-    //I wanted to make it fade out before changing but I need mroe time to figure it out
    
-    /*
+    
     public IEnumerator Fade()
     {
         float elapsed = 0f;
-        float duration = 0.5f;
+        float duration = 0.6f;
 
         while (elapsed < duration)
         {
@@ -62,22 +50,9 @@ public class TitleScreen : MonoBehaviour
             yield return null;
         }
 
-        yield return new WaitForSecondsRealtime(1f);
+        yield return new WaitForSecondsRealtime(.05f);
 
+        SceneManager.LoadScene(1);
 
-        elapsed = 0f;
-
-        while (elapsed < duration)
-        {
-            float t = Mathf.Clamp01(elapsed / duration);
-            fadeImage.color = Color.Lerp(Color.white, Color.clear, t);
-
-            elapsed += Time.unscaledDeltaTime;
-
-            yield return null;
-        }
-
-        SceneManager.LoadScene(0);
-
-    }*/
+    }
 }

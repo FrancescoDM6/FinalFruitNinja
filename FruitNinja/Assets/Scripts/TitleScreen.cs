ec4b8f25949e55b61270349a66e757998f5248f8
FrancescoDM6 <112992086+FrancescoDM6@users.noreply.github.com>
2022-09-29 19:53:10 -0400

    title screen fix

--- FruitNinja/Assets/Scripts/TitleScreen.cs
+++ FruitNinja/Assets/Scripts/TitleScreen.cs
@@ -9,22 +9,17 @@ public class TitleScreen : MonoBehaviour
 {
     public float fov = 130f;
     public float changeMultiplier = .05f;
-    public float endFOV;
+    public float endFOV = 172;
 
     public Image fadeImage;
 
 
     void Start()
     {
-
-        
         Camera.main.fieldOfView = fov;
     }
 
     void Update(){
-        
-      
-
         if (fov < endFOV)
         {
         Camera.main.fieldOfView = fov;
@@ -37,17 +32,16 @@ public class TitleScreen : MonoBehaviour
   }
 
 
-   
-    
     public IEnumerator Fade()
     {
+        yield return new WaitForSecondsRealtime(1.5f);
         float elapsed = 0f;
         float duration = 0.6f;
 
         while (elapsed < duration)
         {
             float t = Mathf.Clamp01(elapsed / duration);
-            // fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
+            fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
 
             Time.timeScale = 1f - t;
             elapsed += Time.unscaledDeltaTime;
